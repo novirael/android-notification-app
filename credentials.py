f9c60892ce6f0714fdad3e99b28e066ac7eca230
novirael <novirael@gmail.com>
2015-05-24 15:22:48 +0200

    Add credentials and main files

---
+++ credentials.py
@@ -0,0 +1,10 @@
+"""
+To use app you have to write down there your allegro
+credentials and generated api key
+http://allegro.pl/webapi
+http://allegro.pl/myaccount/webapi.php/
+"""
+
+API_KEY = 'fake_api_key'
+LOGIN = 'fake_login'
+PASSWORD = 'fake_password'
---
+++ main.py
@@ -0,0 +1,6 @@
+from credentials import API_KEY, LOGIN, PASSWORD
+from webapi import AllegroWebAPI
+
+
+if __name__ == '__main__':
+    AllegroWebAPI(API_KEY, LOGIN, PASSWORD)
